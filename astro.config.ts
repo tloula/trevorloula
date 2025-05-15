5cb5091a2c436c70ef0feeb95c07dd4b51c19cf3
Trevor Loula <trevorloula@outlook.com>
2025-05-14 22:21:26 -0700

    Init GitHub Pages

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import expressiveCode from 'astro-expressive-code';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import spectre from './package/src';
-import googleAnalytics from '@astrojs/google-analytics';
 
 import node from '@astrojs/node';
 import { spectreDark } from './src/ec-theme';
@@ -12,6 +11,7 @@ import { spectreDark } from './src/ec-theme';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://trevorloula.com',
+  base: 'trevorloula',
   output: 'static',
   integrations: [
     expressiveCode({
@@ -19,9 +19,6 @@ export default defineConfig({
     }),
     mdx(),
     sitemap(),
-    googleAnalytics({
-      measurementIds: ['G-MS7W4L99P0'],
-    }),
     spectre({
       name: 'Trevor Loula',
       openGraph: {
