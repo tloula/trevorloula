a100423fdca7e483a767234eaa1e69a194396f5e
Trevor Loula <trevorloula@outlook.com>
2025-06-26 18:41:55 -0700

    Editorial Review

--- src/content/assets/code/telemetry.py
+++ src/content/assets/code/telemetry.py
@@ -3,7 +3,8 @@ Python FastAPI OpenTelemetry Example
 """
 
 import logging
-
+from sqlalchemy import create_engine
+from fastapi import FastAPI
 from opentelemetry import metrics, trace
 from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
@@ -16,38 +17,48 @@ from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+# Variables
+SERVICE_NAME = "my-service"
+SERVICE_VERSION = "1.0.0"
+ENVIRONMENT = "production"
+TRACE_COLLECTOR_ENDPOINT = "http://localhost:4318/v1/traces"
+METRICS_COLLECTOR_ENDPOINT = "http://localhost:4318/v1/metrics"
+
 # Configure Logging
 logger = logging.getLogger(__name__)
 
 # Configure Tracing
 resource = Resource(attributes={
-    "service.name": config.env.service_name,
-    "service.version": config.env.service_version,
-    "service.environment": config.env.environment,
+    "service.name": SERVICE_NAME,
+    "service.version": SERVICE_VERSION,
+    "service.environment": ENVIRONMENT,
 })
 tracer_provider = TracerProvider(resource=resource)
 span_processor = BatchSpanProcessor(
-    OTLPSpanExporter(endpoint=config.env.trace_collector_endpoint),
+    OTLPSpanExporter(endpoint=TRACE_COLLECTOR_ENDPOINT),
 )
 tracer_provider.add_span_processor(span_processor)
+trace.set_tracer_provider(tracer_provider)
 
 # Configure Metrics
 reader = PeriodicExportingMetricReader(
-    OTLPMetricExporter(endpoint=config.env.metrics_collector_endpoint),
+    OTLPMetricExporter(endpoint=METRICS_COLLECTOR_ENDPOINT),
 )
 meter_provider = MeterProvider(resource=resource, metric_readers=[reader])
 metrics.set_meter_provider(meter_provider)
 
 # Instrument the FastAPI app
+app = FastAPI()
 FastAPIInstrumentor.instrument_app(app=app)
 
 # Instrument SQLAlchemy
+engine = create_engine("sqlite:///:memory:")
 SQLAlchemyInstrumentor().instrument(engine=engine)
 
 # Instrument HTTPX
 HTTPXClientInstrumentor().instrument()
 
 logger.info(
-    "OpenTelemetry initialized with service name: '%s', version: '%s', environment: '%s', & .env file: '%s'",
-    config.env.service_name, config.env.service_version, config.env.environment, config.env.loaded_env_file
+    "OpenTelemetry initialized with service name: '%s', version: '%s', environment: '%s'",
+    SERVICE_NAME, SERVICE_VERSION, ENVIRONMENT
 )
